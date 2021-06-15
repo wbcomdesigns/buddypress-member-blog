cdb969bcb11ac54ace2eb48eb9a5c4f163210308
dishantpatel <dishant.patel1007@gmail.com>
2021-06-15 15:15:22 +0530

    add .js  in qp_enqueue_script

--- public/class-buddypress-member-blog-public.php
+++ public/class-buddypress-member-blog-public.php
@@ -96,7 +96,7 @@ class Buddypress_Member_Blog_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-		wp_enqueue_script( 'selectize', plugin_dir_url( __FILE__ ) . 'js/selectize.min', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( 'selectize', plugin_dir_url( __FILE__ ) . 'js/selectize.min.js', array( 'jquery' ), $this->version, false );
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/buddypress-member-blog-public.js', array( 'jquery' ), $this->version, false );
 
 	}	

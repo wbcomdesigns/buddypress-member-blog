a2f770c548f379fb4ffc04b52a04530eddc7df9e
dishantpatel <dishant.patel1007@gmail.com>
2021-06-15 16:03:47 +0530

    add blog container div in post listing

--- templates/posts.php
+++ templates/posts.php
@@ -50,56 +50,59 @@ $query_args = array(
 //do the query
 query_posts( $query_args );
 ?>
+<div  class="bp-member-blog-container">
+	<?php if ( have_posts() ): ?>
 
-<?php if ( have_posts() ): ?>
+		<?php while ( have_posts() ): the_post();
+			global $post;
+		?>
 
-	<?php while ( have_posts() ): the_post();
-		global $post;
-	?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="post-content">
 
-			<div class="post-content">
+					<?php if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( get_the_ID() ) ):?>
 
-				<?php if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( get_the_ID() ) ):?>
+						<div class="post-featured-image">
+							<?php  the_post_thumbnail();?>
+						</div>
 
-					<div class="post-featured-image">
-						<?php  the_post_thumbnail();?>
+					<?php endif;?>
+
+					<h2 class="posttitle"> <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress-member-blog' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h2>
+
+					<p class="date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress-member-blog' ), get_the_date(), get_the_category_list( ', ' ) ); ?></p>
+
+					<div class="entry">
+
+						<?php the_content( __( 'Read the rest of this entry &rarr;', 'buddypress-member-blog' ) ); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress-member-blog' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
 					</div>
 
-				<?php endif;?>
+					<p class="postmetadata"><?php the_tags( '<span class="tags">' . __( 'Tags: ', 'buddypress-member-blog' ), ', ', '</span>' ); ?> <span class="comments"><?php comments_popup_link( __( 'No Comments &#187;', 'buddypress-member-blog' ), __( '1 Comment &#187;', 'buddypress-member-blog' ), __( '% Comments &#187;', 'buddypress-member-blog' ) ); ?></span></p>
 
-				<h2 class="posttitle"> <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress-member-blog' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h2>
-
-				<p class="date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress-member-blog' ), get_the_date(), get_the_category_list( ', ' ) ); ?></p>
-
-				<div class="entry">
-
-					<?php the_content( __( 'Read the rest of this entry &rarr;', 'buddypress-member-blog' ) ); ?>
-					<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress-member-blog' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
+					<?php if ( $action_button == true ): ?>
+						<div class="post-actions">
+							<?php echo bp_member_blog_get_post_publish_unpublish_link( get_the_ID() );?>
+							<?php echo bp_member_blog_get_edit_link();?>
+							<?php echo bp_member_blog_get_delete_link();?>
+						</div>
+					<?php endif; ?>
 				</div>
 
-				<p class="postmetadata"><?php the_tags( '<span class="tags">' . __( 'Tags: ', 'buddypress-member-blog' ), ', ', '</span>' ); ?> <span class="comments"><?php comments_popup_link( __( 'No Comments &#187;', 'buddypress-member-blog' ), __( '1 Comment &#187;', 'buddypress-member-blog' ), __( '% Comments &#187;', 'buddypress-member-blog' ) ); ?></span></p>
-
-				<?php if ( $action_button == true ): ?>
-					<div class="post-actions">
-						<?php echo bp_member_blog_get_post_publish_unpublish_link( get_the_ID() );?>
-						<?php echo bp_member_blog_get_edit_link();?>
-						<?php echo bp_member_blog_get_delete_link();?>
-					</div>
-				<?php endif; ?>
 			</div>
 
-		</div>
+		<?php endwhile;?>
+			<div class="pagination">
+				<?php bp_member_blog_paginate(); ?>
+			</div>
+	<?php else: ?>
+			<p><?php echo sprintf( "<p>%s haven't posted anything yet.</p>", bp_get_displayed_user_fullname() );?></p>
+	<?php endif; ?>
 
-	<?php endwhile;?>
-		<div class="pagination">
-			<?php bp_member_blog_paginate(); ?>
-		</div>
-<?php else: ?>
-		<p><?php echo sprintf( "<p>%s haven't posted anything yet.</p>", bp_get_displayed_user_fullname() );?></p>
-<?php endif; ?>
-
-<?php
-   wp_reset_postdata();
-   wp_reset_query();
+	<?php
+	   wp_reset_postdata();
+	   wp_reset_query();
+	?>
+</div>
+</div>

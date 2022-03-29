7b326f30e1a88ae66126f4fb57a5e10faa9757eb
Wbcom Dev <pallavi@wbcomdesigns.com>
2022-03-29 17:49:39 +0530

    backend UI Updated tab icons

--- admin/class-buddypress-member-blog-admin.php
+++ admin/class-buddypress-member-blog-admin.php
@@ -163,7 +163,7 @@ class Buddypress_Member_Blog_Admin {
 							<?php
 							foreach ( $member_blog_tabs as $bmpro_tab => $bmpro_name ) {
 								$class = ( $bmpro_tab == $current ) ? 'nav-tab-active' : '';
-								echo '<li class="' . esc_html( $bmpro_name ) . '"><a class="nav-tab ' . esc_attr( $class ) . '" href="admin.php?page=buddypress-member-blog&tab=' . esc_attr( $bmpro_tab ) . '">' . esc_html( $bmpro_name ) . '</a></li>';
+								echo '<li id="' . esc_attr( $bmpro_tab ) . '"><a class="nav-tab ' . esc_attr( $class ) . '" href="admin.php?page=buddypress-member-blog&tab=' . esc_attr( $bmpro_tab ) . '">' . esc_html( $bmpro_name ) . '</a></li>';
 							}
 							?>
 							</ul>

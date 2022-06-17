8f98d2581209d77cb45032afc43fa14e903f3eb6
Amit Agrahari <pallavi@wbcomdesigns.com>
2022-06-17 18:23:39 +0530

    grunt

--- admin/inc/bp-member-blog-setting-restriction-tab.php
+++ admin/inc/bp-member-blog-setting-restriction-tab.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="wbcom-tab-content member-blog-pro-tab">
 	<div class="wbcom-welcome-main-wrapper">
 		<a href="<?php echo esc_url( 'https://wbcomdesigns.com/downloads/buddypress-member-blog-pro/' ); ?>" target="_blank">
-			<small><?php esc_html_e( 'Get Pro Version', ' buddypress-member-blog' ); ?></small>
+			<small><?php esc_html_e( 'Get Pro Version', 'buddypress-member-blog' ); ?></small>
 			<img src="<?php echo esc_url( BUDDYPRESS_MEMBER_BLOG_PLUGIN_URL . 'admin/images/restrictions.png' ); ?>">
 		</a>
 	</div>

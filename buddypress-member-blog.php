243770c70257e438493cead3e325f447296ba263
Amit Agrahari <pallavi@wbcomdesigns.com>
2022-11-07 18:55:00 +0530

    Update buddypress-member-blog.php

--- buddypress-member-blog.php
+++ buddypress-member-blog.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Wbcom Designs - BuddyPress Member Blog
  * Plugin URI:        https://wbcomdesigns.com/downloads/buddypress-member-blog
  * Description:       Allow your BuddyPress members to create and manage their blog posts from there profile.  Allow them to publish their posts directly or send them for review. you can easily navigate to any member's profile to read  their posts.
- * Version:           1.9.0
+ * Version:           1.9.1
  * Author:            Wbcom Designs
  * Author URI:        https://wbcomdesigns.com/
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'BUDDYPRESS_MEMBER_BLOG_VERSION', '1.9.0' );
+define( 'BUDDYPRESS_MEMBER_BLOG_VERSION', '1.9.1' );
 
 
 define( 'BUDDYPRESS_MEMBER_BLOG_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
